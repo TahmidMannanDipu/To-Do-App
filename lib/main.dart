fb430b12ac07beb238cd9d483cfd5d7817f70cbd
TahmidMannanDipu <tahmidmannandipu@gmail.com>
2024-11-21 21:11:27 +0600

    final To Do App

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
-import 'package:to_do_app/splash_screen.dart'; // Adjust the path if needed
+import 'package:to_do_app/pages/splash_screen.dart'; // Adjust the path if needed
 
 void main() async {
   // Ensure Flutter is fully initialized before any async operation
